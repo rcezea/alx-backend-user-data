8ce931c81d2b193ab363498bc346cac4ee6a3657
Richard <rcezea007@gmail.com>
2024-05-30 13:47:23 +0100

    readme file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,6 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str):
     """ use a regex to replace occurrences of certain field values. """
-    b = message.split(separator)[0:-1]
     for field in fields:
-        b = [re.sub(rf'^{field}=[A-Za-z0-9_/]+', f"{field}={redaction}", item)
-             if item.startswith(field) else item for item in b]
-    return separator.join(b)
+        message = re.sub(f"(?<={field}=)[^{separator}]*", redaction, message)
+    return message
