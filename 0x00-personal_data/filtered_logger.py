7f76e359017301df1f036dd4bedc014a64aa36f4
Richard <rcezea007@gmail.com>
2024-05-30 13:48:04 +0100

    readme file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
     """ use a regex to replace occurrences of certain field values. """
     for field in fields:
         message = re.sub(f"(?<={field}=)[^{separator}]*", redaction, message)
