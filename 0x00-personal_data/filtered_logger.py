259eeb910b0b1f8a825ee107a4fcce996c79d9e5
Richard <rcezea007@gmail.com>
2024-05-31 05:53:12 +0100

    filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -64,29 +64,26 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     return connection
 
 
-def main() -> None:
-    """The function will obtain a database connection using get_db
-    and retrieve all rows in the users table"""
-    my_logger = get_logger()
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users")
-    for (
-            name,
-            email,
-            phone,
-            ssn,
-            password,
-            ip,
-            last_login,
-            user_agent,
-    ) in cursor:
-        user_details = "name={};email={};phone={};ssn={};"
-        user_details += ("password={};ip={};last_login={};"
-                         "user_agent={};").format(
-            name, email, phone, ssn, password, ip, last_login, user_agent
-        )
-        my_logger.info(user_details)
+def main():
+    """Logs the information about user records in a table.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
 
 if __name__ == "__main__":
