e2db27297e03ee14e5db2d4bfef62fce0199749d
Richard <rcezea007@gmail.com>
2024-06-03 21:01:12 +0100

    Basic Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""Route module for the API.
 """
-import os
+Route module for the API
+"""
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS, cross_origin
+import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
 if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
 
@@ -26,43 +26,37 @@ else:
     auth = None
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """Not found handler.
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler.
-    """
+    """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler.
-    """
+    """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Not found"}), 404
+
+
 @app.before_request
-def authenticate_user():
-    """Authenticates a user before processing a request.
-    """
+def authorize():
+    """authorize a request"""
     if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
+        excluded_path = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
         ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
+        if auth.require_auth(request.path, excluded_path):
+            if not auth.authorization_header(request):
                 abort(401)
-            if user is None:
+            if not auth.current_user(request):
                 abort(403)
 
 
