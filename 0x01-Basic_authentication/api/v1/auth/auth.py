3c6a8157fb56435eb0315c703812392245491b8f
Richard <rcezea007@gmail.com>
2024-06-03 20:58:01 +0100

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,7 @@ from flask import request
 class Auth:
     """ Basic Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-
+        """ Checks if auth is required"""
         if isinstance(path, str) and path:
             slash = str(path) + '/'
             if isinstance(excluded_paths, list) and excluded_paths:
@@ -17,9 +17,11 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
+        """ Authorization header """
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ Current User """
         return None
