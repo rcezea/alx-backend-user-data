2fec004fae9ef2c7bcb737b0ed8eb8a9a12ef804
Richard <rcezea007@gmail.com>
2024-06-11 14:48:22 +0100

    create user class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,3 +18,11 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """ user representation"""
+        return ("<User(id='%s', email='%s', hashed_password='%s',"
+                "session_id='%s', reset_token='%s')>") % (
+                                         self.id, self.email,
+                                         self.hashed_password,
+                                         self.session_id, self.reset_token)
