a6ae59892da03b4de76e08b7939c745ce0bd6f82
Richard <rcezea007@gmail.com>
2024-06-11 15:27:18 +0100

    create user class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python3
-"""
-A user Model class
-"""
+#!/usr/bin/env pyhton3
+""" 0x03. User authentication service """
 
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
-    """User class"""
-
-    __tablename__ = "users"
+    """ User Class """
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
