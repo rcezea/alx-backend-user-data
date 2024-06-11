2ee52da99687272b96d7452d3d909cf0330ada97
Richard <rcezea007@gmail.com>
2024-06-11 21:53:44 +0100

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,3 +39,22 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """ return a specific user """
+        if kwargs:
+            session = self._session
+            for key in kwargs.keys():
+                try:
+                    user_obj = (
+                        session.query(User)
+                        .filter(getattr(User, key) == kwargs.get(key))
+                        .first()
+                    )
+                    if user_obj:
+                        return user_obj
+                except AttributeError:
+                    raise InvalidRequestError
+            raise NoResultFound
+        raise NoResultFound
+
