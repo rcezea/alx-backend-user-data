8b97ea59870fed224ac083db7fe15aa8e9e8d8e5
Richard <rcezea007@gmail.com>
2024-06-11 15:18:12 +0100

    create user class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env pyhton3
 """ 0x03. User authentication service """
 
-
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,16 +12,9 @@ class User(Base):
     create a SQLAlchemy model named User for a database table named users
     """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        """ user representation"""
-        return ("<User(id='%s', email='%s', hashed_password='%s',"
-                "session_id='%s', reset_token='%s')>") % (
-                                         self.id, self.email,
-                                         self.hashed_password,
-                                         self.session_id, self.reset_token)
