95feb223bd6e3c3aa6cb57b9efad54acfbb6412a
Richard <rcezea007@gmail.com>
2024-06-13 16:23:21 +0100

    get reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,7 +71,7 @@ def get_reset_password_token():
             {"email": email, "reset_token": reset_token}
         ), 200
     except ValueError:
-        abort(402)
+        abort(403)
 
 
 if __name__ == "__main__":
