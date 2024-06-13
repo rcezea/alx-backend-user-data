1b3fa1b5f9011dade173869b685ab77dfa3a9bd3
Richard <rcezea007@gmail.com>
2024-06-13 12:00:01 +0100

    validate credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,7 +31,7 @@ class Auth:
             return self._db.add_user(
                 email, hashed_password=_hash_password(password))
 
-    def valid_login(self, email: str, password:str) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """Login Validation"""
         try:
             user = self._db.find_user_by(email=email)
