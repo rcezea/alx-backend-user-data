9ab43a882ae1f9e8aa455a661e8b25437723aa63
Richard <rcezea007@gmail.com>
2024-06-13 15:40:51 +0100

    destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,6 +60,10 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy user session """
+        self._db.update_user(user_id, session_id=None)
+
 
 def _generate_uuid() -> str:
     """Generate UUIDs"""
